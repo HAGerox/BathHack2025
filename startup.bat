3cbfa5e1c10b8a3fe14e1e11f7adb4bdb6a75050
Jamie (dafuq) <46281327+jamieernest@users.noreply.github.com>
2025-04-26 16:10:40 +0100

    fix: remove faulty python path detection

--- startup.bat
+++ startup.bat
@@ -1,6 +1,5 @@
 @ECHO OFF
-FOR /f %%p in ('where python') do SET PYTHONPATH=%%p
-START %PYTHONPATH% backend/server.py
-START %PYTHONPATH% backend/main_backend.py
+START python backend/server.py
+START python backend/main_backend.py
 CD frontend
 START npm run dev
